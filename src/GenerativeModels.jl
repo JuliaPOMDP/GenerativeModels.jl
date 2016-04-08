d019c0a60a4c838f51a0b1ff35fabbc4b912d536
Zachary Sunberg <sunbergzach@gmail.com>
2016-04-08 11:19:24 -0700

    updated documentation

--- src/GenerativeModels.jl
+++ src/GenerativeModels.jl
@@ -6,6 +6,7 @@ export generate_s,
        generate_o,
        generate_sr,
        generate_so,
+       generate_or,
        generate_sor
 
 function generate_s{S}(p::POMDP{S}, s, a, rng::AbstractRNG, sp::S=S())
@@ -28,27 +29,14 @@ function generate_so{S,A,O}(p::POMDP{S,A,O}, s, a, rng::AbstractRNG, sp::S=S(), 
     return sp, generate_o(p, s, a, sp, rng, o)
 end
 
+function generate_or{S,A,O}(p::POMDP{S,A,O}, s, a, sp, rng::AbstractRNG, o::O=O())
+    return generate_o(p, s, a, sp, rng, o), reward(p, s, a, sp)
+end
+
+
 function generate_sor{S,A,O}(p::POMDP{S,A,O}, s, a, rng::AbstractRNG, sp::S=S(), o::O=O())
     sp,o = generate_so(p, s, a, rng, sp, o)
     return sp, o, reward(p, s, a, sp)
 end
-
-#=
-function generate{S,A,O}(p::POMDP{S,A,O}, s::S, a::A, rng::AbstractRNG, sp::S=S(), o::O=O())
-    td = transition(p, s, a)
-    sp = rand(rng, td, sp)
-    od = observation(p, s, a, sp)
-    o = rand(rng, od, o)
-    r = reward(p, s, a, sp)
-    return (sp, o, r)
-end
-
-function generate{S,A}(p::MDP{S,A}, s::S, a::A, rng::AbstractRNG, sp::S=S())
-    td = transition(p, s, a)
-    sp = rand(rng, td, sp)
-    r = reward(p, s, a, sp)
-    return (sp, r)
-end
-=#
 
 end # module
