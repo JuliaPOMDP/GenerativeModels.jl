19a5429633f376fd8d9a04c7db21a0f8180aea96
Zachary Sunberg <sunbergzach@gmail.com>
2016-04-09 13:11:16 -0700

    started using create_functions

--- src/GenerativeModels.jl
+++ src/GenerativeModels.jl
@@ -9,32 +9,32 @@ export generate_s,
        generate_or,
        generate_sor
 
-function generate_s{S}(p::POMDP{S}, s, a, rng::AbstractRNG, sp::S=S())
+function generate_s{S}(p::POMDP{S}, s, a, rng::AbstractRNG, sp::S=create_state(p))
     td = transition(p, s, a)
     return rand(rng, td, sp)
 end
 
-function generate_o{S,A,O}(p::POMDP{S,A,O}, s, a, sp, rng::AbstractRNG, o::O=O())
+function generate_o{S,A,O}(p::POMDP{S,A,O}, s, a, sp, rng::AbstractRNG, o::O=create_observation(p))
     od = observation(p, s, a, sp)
     return rand(rng, od, o)
 end
 
-function generate_sr{S}(p::POMDP{S}, s, a, rng::AbstractRNG, sp::S=S())
+function generate_sr{S}(p::POMDP{S}, s, a, rng::AbstractRNG, sp::S=create_state(p))
     sp = generate_s(p, s, a, rng)
     return sp, reward(p, s, a, sp)
 end
 
-function generate_so{S,A,O}(p::POMDP{S,A,O}, s, a, rng::AbstractRNG, sp::S=S(), o::O=O())
+function generate_so{S,A,O}(p::POMDP{S,A,O}, s, a, rng::AbstractRNG, sp::S=create_state(p), o::O=create_observation(p))
     sp = generate_s(p, s, a, rng, sp)
     return sp, generate_o(p, s, a, sp, rng, o)
 end
 
-function generate_or{S,A,O}(p::POMDP{S,A,O}, s, a, sp, rng::AbstractRNG, o::O=O())
+function generate_or{S,A,O}(p::POMDP{S,A,O}, s, a, sp, rng::AbstractRNG, o::O=create_observation(p))
     return generate_o(p, s, a, sp, rng, o), reward(p, s, a, sp)
 end
 
 
-function generate_sor{S,A,O}(p::POMDP{S,A,O}, s, a, rng::AbstractRNG, sp::S=S(), o::O=O())
+function generate_sor{S,A,O}(p::POMDP{S,A,O}, s, a, rng::AbstractRNG, sp::S=create_state(p), o::O=create_observation(p))
     sp,o = generate_so(p, s, a, rng, sp, o)
     return sp, o, reward(p, s, a, sp)
 end
