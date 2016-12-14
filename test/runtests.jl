fad9b2aad1c45a1e4f6b57d2f6e7d2fdb5bbbc34
Zachary Sunberg <sunbergzach@gmail.com>
2016-12-14 15:36:04 -0800

    before branching to which

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,8 +18,8 @@ transition(b::B, s::Int, a::Bool) = Int[s+a]
 reward(b::B, s::Int, a::Bool, sp::Int) = -1.0
 @test generate_sr(B(), 1, false, Base.GLOBAL_RNG) == (1, -1.0)
 
-generate_o(b::B, s::Int, a::Bool, sp::Int, rng::AbstractRNG) = sp
-generate_sor(B(), 1, true, Base.GLOBAL_RNG) == (1, 2, -1.0)
+# generate_o(b::B, s::Int, a::Bool, sp::Int, rng::AbstractRNG) = sp
+generate_sor(B(), 1, true, Base.GLOBAL_RNG) == (1, 2, -1.0) # should throw sor error
 
 initial_state_distribution(b::B) = Int[1,2,3]
 @test initial_state(B(), Base.GLOBAL_RNG) in initial_state_distribution(B())
